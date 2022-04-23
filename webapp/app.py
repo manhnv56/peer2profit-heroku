4467b80b8d7376b0a2344626361a89ec2a8bb6c7
manhnv56 <manhnv_56@vnu.edu.vn>
2022-04-23 17:26:46 +0700

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -14,7 +14,7 @@ def hello():
     ip = get('https://api.ipify.org').text
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.log")) as f:
         message = f.readlines()[-20:]
-    return f"Hello from: {ip} on: {hostname}." + '<br>' + '<p><a href="https://peer2profit.com/r/164181619061dc207e974f9" rel="noopener noreferrer" target="_blank">Check it</a></p>' + '<br>'.join(message)
+    return f"Hello from: {ip} on: {hostname}." + '<br>' + '<p><a href="https://peer2profit.io/r/164181619061dc207e974f9" rel="noopener noreferrer" target="_blank">Check it</a></p>' + '<br>'.join(message)
 
 
 if __name__ == '__main__':
